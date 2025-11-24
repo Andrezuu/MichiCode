b498aa0ef30a1d135b0b1ae037ee17ad95a5a7e4
marianZamorano <mzamorano295@gmail.com>
2025-11-23 21:34:24 -0400

    mejor por partes

--- backend/src/routes/qrRoutes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { generateQR } from "../controllers/qrController.js";
-
-const router = Router();
-
-router.post("/generateQR", generateQR);
-
-export default router;
