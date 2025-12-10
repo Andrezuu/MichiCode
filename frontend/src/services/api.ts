1faf2828950a23d4f65ddefeb6fb777f29abf269
marianZamorano <mzamorano295@gmail.com>
2025-12-10 14:04:39 -0400

    detallitos

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const url = process.env.REACT_APP_API_BASE_URL || "http://localhost:5000/";
+
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL,
+  baseURL: url,
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
