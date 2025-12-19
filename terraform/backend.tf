8c3ca031a77f9e5835ab98c3837cc342ca16e97e
AndresSanchez <sanchezandres8203@gmail.com>
2025-12-19 15:44:12 -0400

    infra

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "tfstate-tu-nombre-devops" # Requerido por el reto
-    key            = "environments/dev/terraform.tfstate"
+    bucket         = "tfstate-michicode-devops" # Requerido por el reto
+    key            = "michicode/terraform.tfstate"
     region         = "us-west-2"
-    dynamodb_table = "terraform-lock-table" # Para manejo de bloqueos
+    dynamodb_table = "michicode-lock-table" # Para manejo de bloqueos
     encrypt        = true
   }
 }
