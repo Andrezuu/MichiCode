16aed177207fc5cbed5e237530aba0bfc15fcf97
AndresSanchez <sanchezandres8203@gmail.com>
2025-12-19 18:09:56 -0400

    fix

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,3 +2,8 @@ output "public_ip" {
   description = "Public IP de la instancia EC2 (MichiCode)"
   value       = aws_instance.michicode_server.public_ip
 }
+
+output "public_dns" {
+  description = "Public DNS de la instancia EC2"
+  value       = aws_instance.michicode_server.public_dns
+}
